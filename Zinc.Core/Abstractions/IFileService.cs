1b91f104228bcc695ca622f566629a8d30863bf7
niuzhix <35690637@qq.com>
2026-07-23 16:41:06 +0800

    feat(file): 增加文件读/写接口

---
+++ Zinc.Core/Abstractions/IFileService.cs
@@ -0,0 +1,9 @@
+﻿using Zinc.Core.Models;
+
+namespace Zinc.Core.Abstractions;
+
+public interface IFileService
+{
+    string LoadFile(string path);
+    void SaveFile(string path, string content);
+}
---
+++ Zinc.Core/Services/FileService.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using System.Xml.Linq;
+using Zinc.Core.Abstractions;
+using Zinc.Core.Models;
+
+namespace Zinc.Core.Services;
+public class FileService : IFileService
+{
+    public string LoadFile(string filePath)
+    {
+        return File.ReadAllText(filePath);
+    }
+
+    public void SaveFile(string filePath, string content)
+    {
+        File.WriteAllText(filePath, content);
+    }
+}
