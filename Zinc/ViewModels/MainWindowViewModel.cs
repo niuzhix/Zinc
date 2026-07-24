6ae1c1926d95ae1eebac5a452308fa39d8f18885
niuzhix <35690637@qq.com>
2026-07-24 16:38:05 +0800

    feat(UI): 增加菜单栏 feat(UI): 将主窗口继承更变为FAAppWindow feat(file): 增加读入文件测试

--- Zinc/ViewModels/MainWindowViewModel.cs
+++ Zinc/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,10 @@
 ﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Platform.Storage;
 using Avalonia.Styling;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Threading.Tasks;
 using Zinc.Core.Abstractions;
 using Zinc.Core.Models;
 using Zinc.Core.Services;
@@ -9,9 +14,18 @@ namespace Zinc.ViewModels
     public partial class MainWindowViewModel : ViewModelBase
     {
         private readonly ISettingsService _settings;
+        private readonly IFileService _file;
+
+        public static FilePickerFileType CodeAll { get; } = new("All Codes")
+        {
+            Patterns = new[] { "*.cpp", "*.c", "*.hpp", "*.h" },
+            AppleUniformTypeIdentifiers = new[] { "public.code" },
+            MimeTypes = new[] { "code/*" }
+        };
 
         public MainWindowViewModel()
         {
+            _file = new FileService();
             _settings = new SettingsService();
             _settings.Preload();
             ApplyTheme();
@@ -29,6 +43,34 @@ namespace Zinc.ViewModels
                     : (Settings.AppStyle == "Light") 
                     ? ThemeVariant.Light
                     : ThemeVariant.Default;
+            }
+        }
+
+        private Window GetOwner()
+        {
+            if (Avalonia.Application.Current?.ApplicationLifetime
+                is Avalonia.Controls.ApplicationLifetimes.IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                return desktop.MainWindow!;
+            }
+
+            throw new InvalidOperationException("无法获取所有者窗口");
+        }
+
+        [RelayCommand]
+        private async Task OpenFile()
+        {
+            var window = GetOwner();
+            var storage = window.StorageProvider;
+            var files = await storage.OpenFilePickerAsync(new FilePickerOpenOptions()
+            {
+                Title = "选择要打开的文件...",
+                FileTypeFilter = [CodeAll]
+            });
+            if (files.Count > 0)
+            {
+                var code = _file.LoadFile(files[0].Path.LocalPath);
+                Console.WriteLine(code);
             }
         }
     }
--- Zinc/Views/MainWindow.axaml.cs
+++ Zinc/Views/MainWindow.axaml.cs
@@ -1,12 +1,14 @@
 using Avalonia.Controls;
+using FluentAvalonia.UI.Windowing;
 
 namespace Zinc.Views
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : FAAppWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            TitleBar.ExtendsContentIntoTitleBar = true;
         }
     }
 }
