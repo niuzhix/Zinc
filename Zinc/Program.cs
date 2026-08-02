9bf5268b464ee05a2563ec10ab2a0d11f1f39911
niuzhix <35690637@qq.com>
2026-08-02 16:47:52 +0800

    feat(debug): 增加调试控制台窗口

--- Zinc/Program.cs
+++ Zinc/Program.cs
@@ -1,25 +1,83 @@
 ﻿using Avalonia;
 using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
-namespace Zinc
+namespace Zinc;
+
+internal sealed class Program
 {
-    internal sealed class Program
-    {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
-        [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr GetConsoleWindow();
 
-        // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-#if DEBUG
-                .WithDeveloperTools()
-#endif
-                .WithInterFont()
-                .LogToTrace();
+    [DllImport("user32.dll")]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    private const int SW_HIDE = 0;
+    private const int SW_SHOW = 5;
+
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        // Show Debug Console when debugging.
+        if (Debugger.IsAttached || args.Contains("--console"))
+        {
+            ShowConsole();
+        }
+
+        try
+        {
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        }
+        finally
+        {
+            // Hide Debug console when the application exit.
+            if (Debugger.IsAttached)
+            {
+                HideConsole();
+            }
+        }
     }
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+           .UsePlatformDetect()
+#if DEBUG
+           .WithDeveloperTools()
+#endif
+           .WithInterFont()
+           .LogToTrace();
+
+    private static void ShowConsole()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            var handle = GetConsoleWindow();
+            if (handle == IntPtr.Zero)
+            {
+                AllocConsole();
+            }
+            else
+            {
+                ShowWindow(handle, SW_SHOW);
+            }
+        }
+    }
+
+    private static void HideConsole()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            var handle = GetConsoleWindow();
+            if (handle != IntPtr.Zero)
+            {
+                ShowWindow(handle, SW_HIDE);
+            }
+        }
+    }
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern bool AllocConsole();
 }
