fa4878fd5bc4f9fad0e92ca5a489d3a66452322b
niuzhix <35690637@qq.com>
2026-08-02 17:03:37 +0800

    feat(debug): 增加Debug输出

--- Zinc/App.axaml.cs
+++ Zinc/App.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
+using System;
 using System.Linq;
 using Zinc.ViewModels;
 using Zinc.Views;
@@ -18,6 +19,9 @@ namespace Zinc
 
         public override void OnFrameworkInitializationCompleted()
         {
+#if DEBUG
+            LaunchDebugConsole();
+#endif
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
                 desktop.MainWindow = new MainWindow
@@ -27,6 +31,31 @@ namespace Zinc
             }
 
             base.OnFrameworkInitializationCompleted();
+        }
+
+        public static void LaunchDebugConsole()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine(@"
+     ███████╗██╗███╗   ██╗ ██████╗ 
+     ╚══███╔╝██║████╗  ██║██╔════╝
+       ███╔╝ ██║██╔██╗ ██║██║     
+      ███╔╝  ██║██║╚██╗██║██║     
+     ███████╗██║██║ ╚████║╚██████╗
+     ╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝
+            ");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine($"    Zinc Editor v{GetVersion()}\n");
+            Console.WriteLine($"Start time:    {DateTime.Now:yyyy-MM-dd HH:mm:ss}");
+            Console.WriteLine($"Platform:      {Environment.OSVersion.Platform}");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Debug console started.\n");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        private static string GetVersion()
+        {
+            var version = typeof(Program).Assembly.GetName().Version;
+            return version?.ToString() ?? "1.0.0";
         }
     }
 }
